7c7ec1c7dfec0a20a7ae0b0bf4b12062349d5539
axelb <axel.tlth.se>
2024-12-17 12:19:29 +0100

    Fixing bugs in QN.py, now all functions and startingpoints converdes

--- QN.py
+++ QN.py
@@ -22,9 +22,9 @@ def QuasiN(f : Callable[[np.ndarray], float], \
     print("Norm of grad: ", np.linalg.norm(grad_x0))
     print("D_K: ", D_k)
     decendingDir = dk.T @ grad_x0
-    if decendingDir > 0 :
-        D_k = np.eye(D_k.shape[0])
-        dk = -D_k @ grad_x0
+    # if decendingDir > 0 :
+    #     D_k = np.eye(D_k.shape[0])
+    #     dk = -D_k @ grad_x0
     print("dk: ", dk)
     print("x: ", x)
     print("DecendingDir: ", decendingDir)
@@ -34,22 +34,17 @@ def QuasiN(f : Callable[[np.ndarray], float], \
     q_k = grad_c(f,x + p_k) - grad_x0
     print("p_k: ", p_k)
     print("q_k: ", q_k)
+    qTDq = np.inner(q_k, D_k @ q_k)
+    pTq = np.inner(p_k,q_k)
+    ppT = np.outer(p_k,p_k)
     if method == "DFP" :
-        D_k += (p_k@p_k.T / (p_k.T@p_k)) - (D_k @ np.outer(q_k, q_k) @ D_k) / (q_k.T @D_k @q_k)
+        D_k += (ppT / pTq) - ((D_k @ np.outer(q_k, q_k) @ D_k) / qTDq)
     elif method == "BFGS" :
-        pq_dot = p_k.T @ q_k  # Dot product of p_k and q_k
-        if pq_dot <= 0:
-            raise ValueError("pq_dot must be positive to maintain positive definiteness.")
-        qDq = q_k.T @ D_k @ q_k  # q_k^T D_k q_k
-
-        term1 = (1 + qDq / pq_dot) * np.outer(p_k, p_k) / pq_dot
-        term2 = (D_k @ np.outer(q_k, p_k) + np.outer(p_k, q_k) @ D_k) / pq_dot
-
-        D_k = D_k + term1 - term2
-        #D_k = D_k + (1 / (p_k.T @ q_k))*(((1 + (q_k.T @ D_k @ q_k)/(p_k.T @ q_k))*np.outer(p_k,p_k)) - (D_k @ np.outer(q_k, p_k)) - (np.outer(p_k, q_k) @ D_k))
+        qpT = np.outer(q_k,p_k)
+        pqT = np.transpose(qpT)
+        D_k += (1 / pTq)*((1 + (qTDq / pTq))*ppT - (D_k @ qpT) - (pqT @ D_k))
     
     x = x + lam*dk
-        # D_k += (1 / (p_k.T @ q_k))*((1 + (q_k.T @ D_k @ q_k)/(p_k.T @ q_k))*p_k@p_k.T - D_k @ np.outer(q_k, p_k) - np.outer(p_k, q_k) @ D_k)
     
     print("")
     return x, func_eval, lam, np.linalg.norm(grad_c(f,x)), D_k
--- main.py
+++ main.py
@@ -8,11 +8,12 @@ np.set_printoptions(precision=6, suppress=True)
 
 m = lambda lam : 10**(lam - 5)
 i = 1
-def f(x : np.ndarray) -> float:
+
+def f (x : np.ndarray) -> np.ndarray: # with 2 variables: x[0],x[1]
     return x[0]**2 + x[1]**2
-def p (x : np.ndarray) -> np.ndarray:
+def p (x : np.ndarray) -> np.ndarray: # with 2 variables: x[0],x[1]
     return (x[0]-2)**2 + (x[1]-3)**2 + m(i)*(x[0]+x[1]-4)**2+ m(i)*(x[0]-x[1]-1)**2
-def h (x : np.ndarray) -> np.ndarray:
+def h (x : np.ndarray) -> np.ndarray: # with 2 variables: x[0],x[1]
     return (x[0]**2 + x[1] - 11)**2 + (x[0] + x[1]**2 - 7)**2
 def j (x : np.ndarray) -> np.ndarray: # with 4 variables: x[0],x[1],x[2],x[3]
     return (x[0] + 10*x[1])**2 + 5*(x[2]-x[3])**2 + (x[1]-2*x[2])**4 + 10*(x[0]-x[3])**4
@@ -26,16 +27,35 @@ def checkboundaries(x : np.ndarray) -> np.ndarray:
     print((x[0]**3+x[2]**3)+1)
 
 tol = 1e-4
+start_x_2d = np.array([-100,-3231])
 start_x = np.array([1,1,1,-1])
 start_x1 = np.array([1.6,2.3])
 start_x2 = np.array([1.5,2.5])
 start_x3 = np.array([-2,2,2,-1,-1])
 new = np.array([-1.526286 ,1.404612 ,1.367206 ,-1.383988 ,-1.383988])
-x,res,total = Program(j,tol,start_x,"BFGS",True)
-np.set_printoptions(precision=16)  # Set precision to 16 decimal places (or as required)
-print(f"Solution to problem is x = {x}")
-print("f(x) = ",res)
-print("Total function evaluations: ",total)
+if True :
+    x,res,total = Program(h,tol,start_x,"BFGS",False,True)
+    np.set_printoptions(precision=16)  # Set precision to 16 decimal places (or as required)
+    print(f"Solution to problem is x = {x}")
+    print("f(x) = ",res)
+    print("Total function evaluations: ",total)
+
+# print("Testing matrix multiplication")
+
+# A = np.array([1, 2])
+# B = np.array([3, 4])
+# C = A.T @ B
+# D = A @ B.T
+# E = np.outer(B,A)
+# F = np.outer(A,B)
+# print(A)
+# print(B)
+# print(C)
+# print(D)
+# print(E)
+# print(F)
+# G = np.transpose(F)
+# print(G)
 
 def pen() : # Run this to try the penalty problem
     i = 0
